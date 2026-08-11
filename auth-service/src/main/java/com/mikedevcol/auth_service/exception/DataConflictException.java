131bdbd9c5ccd62a772b8a0002af3e1c2ac113d7
Emanuel Obredor <obredoremanuel@gmail.com>
2026-08-11 13:56:47 -0500

    Revert "feat (auth-service): R001 - user register"

--- auth-service/src/main/java/com/mikedevcol/auth_service/exception/DataConflictException.java
+++
@@ -1,9 +0,0 @@
-package com.mikedevcol.auth_service.exception;
-
-public class DataConflictException extends RuntimeException {
-
-  public DataConflictException(String message) {
-    super(message);
-  }
-
-}
